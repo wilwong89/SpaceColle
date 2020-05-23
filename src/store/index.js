885c69f12167bcd4f2773ba26fb341e7b9456896
wilwong89 <wilwong89@gmail.com>
2020-05-23 02:08:56 -0700

    Setting up UI and player data for vuex

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,16 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import Ships from "./Ships";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
+  state: {
+    ui: {
+      darkMode: false,
+      sidebarPosition: 1, // 1 for left, 2 for right
+    },
+    playerInfo: {},
+    Ships,
+  },
 });
